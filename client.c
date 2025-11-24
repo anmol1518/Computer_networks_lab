285daa835fd6fe5c66d5ac6aa1539d45e3388b42
anmol1518 <mathuranmol333@gmail.com>
2025-11-25 00:50:27 +0530

    Initial upload

---
+++ client.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+
+int main() {
+    int sock = 0;
+    struct sockaddr_in serv_addr;
+    char buffer[1024] = {0};
+    const char *hello = "Hello from client!";
+
+    // Create socket
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("Socket creation error\n");
+        return -1;
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    // Convert IPv4 address from text to binary
+    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+        printf("Invalid address\n");
+        return -1;
+    }
+
+    // Connect to server
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        printf("Connection failed\n");
+        return -1;
+    }
+
+    // Send message
+    send(sock, hello, strlen(hello), 0);
+    printf("Message sent\n");
+
+    // Read response
+    read(sock, buffer, 1024);
+    printf("Server replied: %s\n", buffer);
+
+    close(sock);
+    return 0;
+}
